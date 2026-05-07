8497e0df7e2ce768861579f2526a097e4e8a8d7d
fumnanya92 <fumnanya541@gmail.com>
2026-05-07 08:34:19 +0100

    feat: add Sam v2 diff summary

--- sam_v2/projects/__init__.py
+++ sam_v2/projects/__init__.py
@@ -1,10 +1,14 @@
 """Project awareness helpers for Sam v2."""
 
+from .diff_summary import DiffFileSummary, DiffSummary, DiffSummaryService
 from .failure_analysis import CommandFailureAnalysis, FailureAnalysisService, resolve_flutter_command
 from .inspector import ProjectInspection, ProjectInspector, inspection_metadata
 from .registry import ProjectRecord, ProjectRegistry
 
 __all__ = [
+    "DiffFileSummary",
+    "DiffSummary",
+    "DiffSummaryService",
     "CommandFailureAnalysis",
     "FailureAnalysisService",
     "ProjectInspection",
